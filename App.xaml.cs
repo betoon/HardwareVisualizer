bb4242f0ce1da0b67a09cfe757e68f3220e08349
betoon <56229671+betoon@users.noreply.github.com>
2026-08-18 20:34:44 -0400

    splash screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,5 +9,16 @@ namespace HardwareVisualizer;
 /// </summary>
 public partial class App : Application
 {
-}
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
 
+        var splash = new SplashScreen("Assets/splash.png");
+        splash.Show(autoClose: false);
+
+        var window = new MainWindow();
+        MainWindow = window;
+        window.Loaded += (_, _) => splash.Close(TimeSpan.FromMilliseconds(350));
+        window.Show();
+    }
+}
